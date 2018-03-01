9824404c86b9f91c875bc7ec4c1d1c400748d74c
屈中亮 <617969695@qq.com>
2018-03-01 11:03:34 +0800

    最新修改

--- GitTest/TestViewController.m
+++ GitTest/TestViewController.m
@@ -25,14 +25,8 @@
     NSLog(@"有输出啊");
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)ceatUI{
+    NSLog(@"最新的1");
 }
-*/
 
 @end
