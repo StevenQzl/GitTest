6577fe760dffe3fc6ef6929811d127546ebfd42e
StevenQzl <m18103842369@163.com>
2018-03-01 10:14:07 +0800

    提交修改

--- GitTest/TestViewController.m
+++ GitTest/TestViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    NSLog(@"哈哈")；
+    NSLog(@"哈哈");
 }
 
 - (void)didReceiveMemoryWarning {
